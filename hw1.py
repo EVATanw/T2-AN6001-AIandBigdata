f38bb3c64a9cfdb52aceef919b2bcba8c7a070ed
EVATanw <158056436+EVATanw@users.noreply.github.com>
2024-12-02 03:17:08 +0000

    1

--- hw1.py
+++ hw1.py
@@ -11,6 +11,22 @@ def HWagenda():
 def HWq1():
     return render_template("HWq1.html")
 
+@app.route("/HWq2", methods=["GET", "POST"])
+def HWq2():
+    return render_template("HWq2.html")
+
+@app.route("/HWq3", methods=["GET", "POST"])
+def HWq3():
+    return render_template("HWq3.html")
+
+@app.route("/HWq4", methods=["GET", "POST"])
+def HWq4():
+    return render_template("HWq4.html")
+
+@app.route("/HWq5", methods=["GET", "POST"])
+def HWq5():
+    return render_template("HWq5.html")
+
 if __name__ == "__main__":
     app.run()
 
